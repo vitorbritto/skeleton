73739360d4b773321b146d84eff5b59139e79b07
Vitor Britto <code@vitorbritto.com.br>
2013-10-24 16:04:57 -0200

    Initial Commit

--- init.bat
+++ init.bat
@@ -7,19 +7,19 @@ mkdir .\assets\js
 mkdir .\assets\js/vendor
 
 echo "Copying files..."
-cp .\template\.editorconfig .\.editorconfig
-cp .\template\.gitignore .\.gitignore
-cp .\template\.jshintrc .\.jshintrc
-cp .\template\Gruntfile.js .\Gruntfile.js
-cp .\template\humans.txt .\humans.txt
-cp .\template\index.html .\index.html
-cp .\template\package.json .\package.json
-cp .\template\robots.txt .\robots.txt
-cp .\template\assets\css\style.css .\assets\css\style.css
-cp .\template\assets\css\normalize.css .\assets\css\normalize.css
-cp .\template\assets\js\main.js .\assets\js\main.js
-cp .\template\assets\js\vendor\jquery-1.9.1.min.js .\assets\js\vendor\jquery-1.9.1.min.js
-cp .\template\assets\js\vendor\modernizr-2.6.2.min.js .\assets\js\vendor\modernizr-2.6.2.min.js
+copy .\template\.editorconfig .\.editorconfig
+copy .\template\.gitignore .\.gitignore
+copy .\template\.jshintrc .\.jshintrc
+copy .\template\Gruntfile.js .\Gruntfile.js
+copy .\template\humans.txt .\humans.txt
+copy .\template\index.html .\index.html
+copy .\template\package.json .\package.json
+copy .\template\robots.txt .\robots.txt
+copy .\template\assets\css\style.css .\assets\css\style.css
+copy .\template\assets\css\normalize.css .\assets\css\normalize.css
+copy .\template\assets\js\main.js .\assets\js\main.js
+copy .\template\assets\js\vendor\jquery-1.9.1.min.js .\assets\js\vendor\jquery-1.9.1.min.js
+copy .\template\assets\js\vendor\modernizr-2.6.2.min.js .\assets\js\vendor\modernizr-2.6.2.min.js
 
 echo "Removing stuff you don't want..."
 del /S /F .git
